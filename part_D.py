09e33efb4b788b91520e35e47d8ed5759d414846
Amr M. Kayid <amrkayid2027@gmail.com>
2018-04-06 01:49:22 +0200

    Finish Part D

---
+++ part_D.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+import psycopg2
+
+conn = psycopg2.connect(database="db2", user = "postgres", password = "123", host = "127.0.0.1", port = "5432")
+print("Opened database successfully")
+cur = conn.cursor()
+
+
+cur.execute('''DROP INDEX cup_yearIndx; DROP INDEX playedin_yearIndx;''')
+conn.commit()
+
+print("D.Q1")
+cur.execute('''EXPLAIN SELECT * from cup_matches, played_in WHERE cup_matches.year=played_in.year;''')
+print(cur.fetchall())
+conn.commit()
+
+
+print("\nD.Q2")
+cur.execute('''CREATE INDEX cup_yearIndx ON cup_matches (year);''')
+conn.commit()
+print("After cup_yearIndx")
+cur.execute('''EXPLAIN SELECT * from cup_matches, played_in WHERE cup_matches.year=played_in.year;''')
+print(cur.fetchall())
+conn.commit()
+
+print("\nD.Q3")
+print("")   ## TODO: Explain => Different
+
+
+print("\nD.Q4")
+cur.execute('''CREATE INDEX playedin_yearIndx ON played_in (year);''')
+conn.commit()
+print("After playedin_yearIndx")
+cur.execute('''EXPLAIN SELECT * from cup_matches, played_in WHERE cup_matches.year=played_in.year;''')
+print(cur.fetchall())
+conn.commit()
+
+
+print("\nD.Q5")
+print("")   ## TODO: Explain => Different
